2df34e43e591c6cb601cb7c74a7e666f7b7ebb83
sgeorge-nb <122819133+sgeorge-nb@users.noreply.github.com>
2023-09-10 12:24:56 +0530

    Update entrypoint.sh

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# ansible-galaxy install -r roles/requirements.yml
-
-echo $ANSIBLE_VAULT_PASSWORD >> .vault
+# ansible-galaxy install -r roles/requirements.yml 
 
 cd 05-dynamic-inventories/
+
+echo $ANSIBLE_VAULT_PASSWORD >> .vault
 
 ansible-playbook -i hosts_azure_rm.yml site.yml --vault-password-file .vault
 
