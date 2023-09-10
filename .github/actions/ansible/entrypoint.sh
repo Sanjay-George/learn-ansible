95a2ea9ecf73e7dd87512aec568cbbfa0e9e22ee
sgeorge-nb <122819133+sgeorge-nb@users.noreply.github.com>
2023-09-10 09:39:51 +0530

    Add action to deploy ansible

---
+++ .github/actions/ansible/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ansible-galaxy install -r roles/requirements.yml
+
+echo $ANSIBLE_VAULT_PASSWORD >> .vault
+
+cd 05-dynamic-inventories/
+
+ansible-playbook -i hosts_azure_rm.yml site.yml --vault-password-file .vault
+
+rm .vault
